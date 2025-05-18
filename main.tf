6f81dc01254b0e7cc168f6cd94bb2fff59452cd6
Omar Mejdi <omar.mejdi@insat.ucar.tn>
2025-05-18 22:40:43 +0100

    added security group and ssh keys

--- main.tf
+++ main.tf
@@ -1,10 +1,45 @@
 provider "aws" {
 region = "us-east-1"
 }
+resource "aws_key_pair" "mykey" {
+  key_name   = "deployer-key"
+  public_key = file("C:/Users/Omar/.ssh/id_rsa.pub") # Adjust path as needed
+}
+
+
 resource "aws_instance" "web" {
-    ami = "ami-085386e29e44dacd7" 
+    ami           = "ami-085386e29e44dacd7"
     instance_type = "t2.micro"
     tags = {
-        Name = "DevOps-Instance"
-    }
+        Name = "Best-EC2-Container"
+    }  
+    key_name  = aws_key_pair.mykey.key_name
+
+    vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
